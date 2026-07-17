909b91e142b69596bd6a5d2fc6b11c3a4fb4caaf
Shubhamd13 <30868812+Shubhamd13@users.noreply.github.com>
2026-07-17 01:25:19 +0000

    Deploying to gh-pages from @ Shubhamd13/shubhamd13.github.io@6d612a2f88fc0ed854f2672fa87eec853c9a72b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "Selected robotics and machine learning projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-acknowledgments",
+          title: "Acknowledgments",
+          description: "Research papers where I am acknowledged for contributions but not listed as an author.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/acknowledged-contributions/";
           },
         },{id: "nav-cv",
           title: "CV",
